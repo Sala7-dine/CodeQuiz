7181d04c103b3cd5790bfcbe1eaa814409a5eee7
salahdine <salahdaha7@gmail.com>
2024-11-05 20:22:45 +0100

    design home page

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/*.{html,js}"],
+  theme: {
+    extend: {
+
+      opacity: {
+        '98': '0.99',
+       } , 
+
+       backgroundImage: {
+        'shape': "url('/images/shapes.svg')",
+      }
+    },
+  },
+  plugins: [],
+}
+
